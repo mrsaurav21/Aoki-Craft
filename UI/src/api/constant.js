ddb3298db4e77c6f92bdf2a3914527f0cd68a612
saurav singh <natasha.singh270800@gmail.com>
2026-01-04 00:26:23 +0530

    fix oauth2 login configuration

--- UI/src/api/constant.js
+++ UI/src/api/constant.js
@@ -6,7 +6,8 @@ export const API_URLS = {
     GET_CATEGORY: (id) => `/api/category/${id}`,
 }
 
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 
 export const getHeaders = ()=>{
